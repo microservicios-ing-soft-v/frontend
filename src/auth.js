1d152ca2b18fae2f20160095bc10070ee9d22c57
Santiago Barraza <santiago.barraza1@u.icesi.edu.co>
2025-04-28 02:13:41 -0500

    Update auth.js

--- src/auth.js
+++ src/auth.js
@@ -7,7 +7,7 @@ import decode from 'jwt-decode'
  * @var{string} LOGIN_URL The endpoint for logging in. This endpoint should be proxied by Webpack dev server
  *    and maybe nginx in production (cleaner calls and avoids CORS issues).
  */
-const LOGIN_URL = window.location.protocol + '//' + window.location.host + '/login'
+const LOGIN_URL = process.env.AUTH_API_ADDRESS
 const ROLE_ADMIN = 'ADMIN'
 
 /**
